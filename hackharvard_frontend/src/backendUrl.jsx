9a7100fc0737283a344489495e7885ed4c170579
13shreyansh <105847464+13shreyansh@users.noreply.github.com>
2026-05-03 15:45:31 +0800

    Use same-origin socket URL on deploy

--- hackharvard_frontend/src/backendUrl.jsx
+++ hackharvard_frontend/src/backendUrl.jsx
@@ -1,4 +1,8 @@
-const backend_port = 5001
+const local_backend_url = "http://" + window.location.hostname + ":5001/";
+const same_origin_backend_url = window.location.origin + "/";
 
-export const backend_url = "http://" + window.location.hostname + ":" + backend_port + "/";
-
+export const backend_url =
+  import.meta.env.VITE_BACKEND_URL ||
+  (window.location.port && window.location.port !== "5001"
+    ? local_backend_url
+    : same_origin_backend_url);
